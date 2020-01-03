e48caf80ec5fc5de8c1c8c6dcfce40afb22fa008
decoded-cipher <arjun12345krishna@gmail.com>
2020-01-04 03:34:06 +0530

    A failed attenpt

--- app/src/main/java/com/example/test_project/LoginActivity.java
+++ app/src/main/java/com/example/test_project/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.test_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +12,7 @@ import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText username, password;
+    private EditText username, password;
     Button login;
     String usrname, passwrd, user, pass;
     Intent log1;
@@ -39,6 +40,9 @@ public class LoginActivity extends AppCompatActivity {
                 if (user.equals(usrname) && pass.equals(passwrd)) {
 //                    Toast.makeText(getApplicationContext(), "Credentials Accepted!", Toast.LENGTH_LONG).show();
                     Intent log1 = new Intent(getApplicationContext(),LoginResult.class);
+
+                    SharedPreferences sh = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+
                     b1 = new Bundle();
                     b1.putString("username",user);
 
